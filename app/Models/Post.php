3adfb63a197f3e64b79669fbce8e416bd12c3e27
Kneeba921 <j.knee@live.com>
2025-12-04 15:13:52 -0330

    Add Post model, factory, and migration for managing posts

---
+++ app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+  /** @use HasFactory<\Database\Factories\PostFactory> */
+  use HasFactory;
+
+  protected $fillable = [
+    'parent_id',
+    'profile_id',
+    'content'
+  ];
+
+  public function profile()
+  {
+    return $this->belongsTo(Profile::class);
+  }
+
+  public function parent()
+  {
+    return $this->belongsTo(Post::class, 'parent_id');
+  }
+
+  public function replies()
+  {
+    return $this->hasMany(Post::class, 'parent_id');
+  }
+}
